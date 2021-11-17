94166d32b627d406e26e13dd913c67eeb1e6afca
Lucas GIRARD <lucasgirard1605@gmail.com>
2021-11-17 10:46:41 +0100

    added bit strings

---
+++ Bit-Strings.cpp
@@ -0,0 +1,25 @@
+/**
+ * Lucas GIRARD, L3 informatique Universite Sciences et Techniques de Nantes
+ * <lucas.girard@etu.univ-nantes.fr>
+ * <lucasgirard1605@gmail.com>
+*/
+
+/*
+    two set https://cses.fi/problemset/task/1092
+*/
+
+#include <iostream>
+#include <math.h>
+
+int main(){
+    unsigned long long n, res(2);
+    std::cin >> n;
+
+    //pow too slow, manualy compute 2 power n
+    for(unsigned i(1); i<n; i++){
+        res <<= 1;
+        res %= 1000000007;
+    }
+
+    std::cout << res;
+}
