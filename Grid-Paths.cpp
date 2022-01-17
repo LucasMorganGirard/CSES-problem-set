dd127fd7bc9db0264ada570d3700f3f60e298f85
Lucas GIRARD <lucasgirard1605@gmail.com>
2022-01-17 18:12:18 +0100

    version 2, not much better

--- Grid-Paths.cpp
+++ Grid-Paths.cpp
@@ -51,10 +51,10 @@ private:
 		line completness count
 		if a column/line is complete (=MATRICE_SIZE), any column/line on the side opposite to the end
 		of the complete column/line must be complete too cause there is no path anymore to the other
-		side of the complete column. Column 0 and line MATRICE SIZE are not counted
+		side of the complete column.
 	*/
-	size_t columnCount[MATRICE_SIZE-1];
-	size_t lineCount[MATRICE_SIZE-1];
+	size_t columnCount[MATRICE_SIZE];
+	size_t lineCount[MATRICE_SIZE];
 
 public:
 	/*
@@ -68,10 +68,17 @@ public:
 			}
 		}
 
+		for (size_t i = 0; i < MATRICE_SIZE; i++){
+			columnCount[i] = 0;
+			lineCount[i] = 0;
+		}
+
 		/*
 			set starting case to visited
 		*/
 		this->visited[0][0] = 1;
+		this->columnCount[0]++;
+		this->lineCount[0]++;
 	}
 
 	grid(grid& previousStep, size_t currentX, size_t currentY) : nbCaseVisited(previousStep.nbCaseVisited + 1), currentX(currentX), currentY(currentY){
@@ -83,11 +90,18 @@ public:
 				this->visited[y][x] = previousStep.visited[y][x];
 			}
 		}
+		
+		for (size_t i = 0; i < MATRICE_SIZE; i++){
+			columnCount[i] = previousStep.columnCount[i];
+			lineCount[i] = previousStep.lineCount[i];
+		}
 
 		/*
 			set current location to visited
 		*/
 		this->visited[currentY][currentX] = 1;
+		this->columnCount[currentX]++;
+		this->lineCount[currentY]++;
 	}
 
 	size_t getCurrentX() const{
@@ -105,28 +119,94 @@ public:
 	bool isFreeUp() const{
 		bool result(false);
 		if(currentY > 0){
-			result = !visited[currentY - 1][currentX];
+			if(!visited[currentY - 1][currentX]){
+				result = true;
+			}
+
 		}
 		return result;
 	}
+
 	bool isFreeRight() const{
 		bool result(false);
 		if(currentX < MATRICE_SIZE - 1){
-			result = !visited[currentY][currentX + 1];
+			if(!visited[currentY][currentX + 1]){
+				result = true;
+			}
 		}
 		return result;
 	}
+
 	bool isFreeDown() const{
 		bool result(false);
 		if(currentY < MATRICE_SIZE - 1){
-			result = !visited[currentY + 1][currentX];
+			if(!visited[currentY + 1][currentX]){
+				result = true;
+			}
 		}
 		return result;
 	}
+
 	bool isFreeLeft() const{
 		bool result(false);
 		if(currentX > 0){
-			result = !visited[currentY][currentX - 1];
+			if(!visited[currentY][currentX - 1]){
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @brief check if adding a path in a specific line will block any other line from completing a path
+	 * exemple : if line 2 is completed, line 1 and line 0 must be complete in order to have a valid path
+	 * that goes trough every case.
+	 * 
+	 * @param y line number
+	 * @return true if one case can be used in line @y without blocking an other line
+	 */
+	bool validCompletLine(size_t y) const{
+		bool result(true);
+		/*
+			test if line[y] would be complete (=MATRICE_SIZE - 1)
+			if so, verify that all line opposite to the end are complete
+		*/
+		if(this->lineCount[y] + 1 == MATRICE_SIZE){
+			/*
+				test if all line opposite to the new complete one are complete
+			*/
+			for(int i(y - 1); i >= 0; i--){
+				if(lineCount[i] < MATRICE_SIZE){
+					result = false;
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @brief check if adding a path in a specific column will block any other column from completing a path
+	 * exemple : if column 2 is completed, column 1 and column 0 must be complete in order to have a valid path
+	 * that goes trough every case.
+	 * 
+	 * @param x column number
+	 * @return true if one case can be used in column @x without blocking an other column
+	 */
+	bool validCompletColumn(size_t x) const{
+		bool result(true);
+		/*
+			test if column[x] would be complete (=MATRICE_SIZE - 1)
+			if so, verify that all column opposite to the end are complete
+		*/
+		if(this->columnCount[x] + 1 == MATRICE_SIZE){
+			/*
+				test if all column opposite to the new complete one are complete
+			*/
+			for(int i(x + 1); i < MATRICE_SIZE; i++){
+				if(columnCount[i] < MATRICE_SIZE){
+					result = false;
+				}
+			}
 		}
 		return result;
 	}
@@ -134,7 +214,12 @@ public:
 	/*
 		debug only
 	*/
-	void printPath(){
+	void printPath() const{
+		std::cout << "(" << this->currentX << ", " << this->currentY << ")\n";
+		for(size_t x(0); x < MATRICE_SIZE; x++){
+			std::cout << columnCount[x] << " ";
+		}
+		std::cout << "\n";
 		for(size_t y(0); y < MATRICE_SIZE; y++){
 			for(size_t x(0); x < MATRICE_SIZE; x++){
 				if(visited[y][x]){
@@ -143,7 +228,7 @@ public:
 					std::cout << "O ";
 				}
 			}
-			std::cout << "\n";
+			std::cout << lineCount[y] << "\n";
 		}
 		std::cout << "\n";
 	}
@@ -151,9 +236,10 @@ public:
 
 void countPath(grid currentPath, size_t& accumulateur){
 	/*
-		test if 
+		test if current (x, y) are at the end point (0, MATRICE_SIZE - 1) and if they are test if
+		all cases have been visited by current path
 	*/
-	if(currentPath.getCurrentY() == MATRICE_SIZE-1 && currentPath.getCurrentX() == 0){
+	if(currentPath.getCurrentY() == MATRICE_SIZE - 1 && currentPath.getCurrentX() == 0){
 		if(currentPath.getNbCaseVisited() == TOTAL_CASES){
 			accumulateur++;
 		}
@@ -207,20 +293,28 @@ void countPath(grid currentPath, size_t& accumulateur){
 		*/
 		if(!(up && down && !right && !left)&&!(!up && !down && right && left)){
 			if(up){
-				grid nextSepTop(currentPath, currentPath.getCurrentX(), currentPath.getCurrentY() - 1);
-				countPath(nextSepTop, accumulateur);
+				if(currentPath.validCompletColumn(currentPath.getCurrentX()) && currentPath.validCompletLine(currentPath.getCurrentY() - 1)){
+					grid nextSepTop(currentPath, currentPath.getCurrentX(), currentPath.getCurrentY() - 1);
+					countPath(nextSepTop, accumulateur);
+				}
 			}
 			if(right){
-				grid nextSepTop(currentPath, currentPath.getCurrentX() + 1, currentPath.getCurrentY());
-				countPath(nextSepTop, accumulateur);
+				if(currentPath.validCompletColumn(currentPath.getCurrentX() + 1) && currentPath.validCompletLine(currentPath.getCurrentY())){
+					grid nextSepTop(currentPath, currentPath.getCurrentX() + 1, currentPath.getCurrentY());
+					countPath(nextSepTop, accumulateur);
+				}
 			}
 			if(down){
-				grid nextSepTop(currentPath, currentPath.getCurrentX(), currentPath.getCurrentY() + 1);
-				countPath(nextSepTop, accumulateur);
+				if(currentPath.validCompletColumn(currentPath.getCurrentX()) && currentPath.validCompletLine(currentPath.getCurrentY() + 1)){
+					grid nextSepTop(currentPath, currentPath.getCurrentX(), currentPath.getCurrentY() + 1);
+					countPath(nextSepTop, accumulateur);
+				}
 			}
 			if(left){
-				grid nextSepTop(currentPath, currentPath.getCurrentX() - 1, currentPath.getCurrentY());
-				countPath(nextSepTop, accumulateur);
+				if(currentPath.validCompletColumn(currentPath.getCurrentX() - 1) && currentPath.validCompletLine(currentPath.getCurrentY())){
+					grid nextSepTop(currentPath, currentPath.getCurrentX() - 1, currentPath.getCurrentY());
+					countPath(nextSepTop, accumulateur);
+				}
 			}
 		}
 	}
