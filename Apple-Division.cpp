2f18a7c9dd2301b8a23fd20ece41a541b7d7b5f6
Lucas GIRARD <lucasgirard1605@gmail.com>
2022-01-26 01:08:44 +0100

    Apple division version 1, working in 0,01s

--- Apple-Division.cpp
+++ Apple-Division.cpp
@@ -5,45 +5,56 @@
 */
 
 /*
-	creating strings https://cses.fi/problemset/task/1622
+	Apple Division https://cses.fi/problemset/task/1623
 */
 
 #include <iostream>
-#include <math.h>
 
-//we need all combinaison of all subset
+const size_t MAX(20);
+
+size_t N(0);
+size_t WEIGHT[MAX];
+size_t BEST(SIZE_MAX);
+size_t CURRENT_SUM(0);
+size_t TOTAL_SUM(0);
+
+void calculAllDifference(size_t i){
+	/*
+		if i = N-1 we are at the endpoint and have succesfully calculated one combinaison of apple in groupe 1
+		we then test if the difference with groupe 2 is better that the current best. 
+		current difference = |groupe_1_sum - (total_sum - groupe_1_sum)|
+	*/
+	if(i == N-1){
+		size_t currentDiff;
+		if(CURRENT_SUM > TOTAL_SUM/2){
+			currentDiff = CURRENT_SUM - (TOTAL_SUM - CURRENT_SUM);
+		}else{
+			currentDiff = (CURRENT_SUM - CURRENT_SUM) - CURRENT_SUM;
+		}
+		BEST = std::min(BEST, currentDiff);
+		return;
+	}
+
+	/*
+		create all combinaison for groupe 1, generate one sum with current apple i and one without it.
+	*/
+	CURRENT_SUM+=WEIGHT[i];
+	calculAllDifference(i+1);
+	CURRENT_SUM-=WEIGHT[i];
+	calculAllDifference(i+1);
+}
 
 int main()
 {
-	unsigned n, sum(0), nPow2, currentSum(0), weights[20], bestSum(0), halfSum;
+	size_t n, totalSum(0), currentSum(0), bestDiff(SIZE_MAX);
 	std::cin >> n;
 
-	for (unsigned short i(0); i < n; i++)
-	{
-		std::cin >> weights[i], sum += weights[i];
+	for(size_t i(0); i<n; i++){
+		std::cin >> WEIGHT[i];
+		totalSum+=WEIGHT[i];
 	}
-	halfSum = sum / 2.0;
 
-	//one loop for each subset
-
-	nPow2 = 1 << n;
-
-	for (unsigned j(0); j < nPow2; j++)
-	{
-		currentSum = 0;
-		for (unsigned short k(0); k < n; k++)
-		{
-			if ((j >> k) & 1)
-			{
-				currentSum += weights[k];
-			}
-		}
-		if (currentSum <= halfSum)
-		{
-			bestSum = std::max(bestSum, currentSum);
-		}
-	}
-	std::cout << sum - bestSum * 2;
-
+	calculAllDifference(0);
+	std::cout << BEST;
 	return 0;
 }
